1317bab727a6ee81bacb80955e6082d86dfa770a
Sayali <sayli31@gmail.com>
2021-07-02 13:21:24 +0530

    Test4

---
+++ src/test/java/com/selenium/may/SeleniumFirstGit/Test4.java
@@ -0,0 +1,26 @@
+package com.selenium.may.SeleniumFirstGit;
+
+public class Test4 {
+	
+	public static void main(String[] args) {
+		
+		int i = 10;
+		String name ="JAVA";
+		String city ="Pune";
+		String collgname ="";
+		int k =10;
+		int id = 100;
+		System.out.println("ID" +i);
+		
+		
+
+		
+		
+	}
+	
+	public void m1()
+	{
+		
+	}
+
+}
